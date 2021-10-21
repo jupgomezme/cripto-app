b39e83ab2f04e1e2033e1c48a0531746e3fb09f2
aerubianoma <aerubianoma@unal.edu.co>
2021-10-21 11:33:58 -0500

    displacement+frequencyTable
    I add the displacement encryption (Cesar's encryption) and the first aproximation to a frequency table

---
+++ api/displacement.py
@@ -0,0 +1,18 @@
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def cesarEncryption(plain_text,key):
+    encrypted_text = ''
+    for i in range(len(plain_text)):
+        position = letters.find(plain_text[i])
+        new_position = (position + key) % 26
+        encrypted_text += letters[new_position]
+    return encrypted_text
+print(cesarEncryption('HELLOWORLD',3))
+def cesarDecryption(plain_text,key):
+    encrypted_text = plain_text
+    decrypted_text = ''
+    for i in range(len(encrypted_text)):
+        position = letters.find(encrypted_text[i])
+        new_position = (position - key) % 26
+        decrypted_text += letters[new_position]
+    return decrypted_text
+print(cesarDecryption('KHOORZRUOG',3))
---
+++ api/frequencyTable.py
@@ -0,0 +1,12 @@
+import numpy as np
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def printFrequencyTable(plain_text):
+    frequencyTable = np.zeros(26) 
+    for i in range(len(plain_text)):
+        position = letters.find(plain_text[i])
+        frequencyTable[position] += 1
+    frequencyTable = frequencyTable * 1/26
+    print('Letter   |   Frequency')
+    for i in range(len(frequencyTable)):
+        print('   '+letters[i]+'     |   '+str(frequencyTable[i]))
+printFrequencyTable('HELLOWORLD')
