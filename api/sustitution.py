eae9e2ef962f176ffea594721b8e322f563e07d2
aerubianoma <aerubianoma@unal.edu.co>
2021-10-29 22:09:08 -0500

    hill and sustitution encryption
    I add 2 new method for encryption, hill and sustitution, also I change de inverse mod in multiplication, it had some errors

--- api/sustitution.py
+++ api/sustitution.py
@@ -2,38 +2,39 @@ import random
 
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def affineEncryptionWithKey(plain_text,key):
-    encrypted_text = ''      
-    for i in range(len(plain_text)):
-        if plain_text[i]==' ':
-            encrypted_text += ' '
-        else:
-            position = letters.find(plain_text[i])
-            new_position = (key[0]*position + key[1]) % 26
-            encrypted_text += letters[new_position]
-    return encrypted_text,key
-
-def affineEncryptionNoKey(plain_text):
+def sustitutionEncryptionWithKey(plain_text,key):
     encrypted_text = ''
-    key = [random.randint(1, 26),random.randint(1, 26)]      
     for i in range(len(plain_text)):
         if plain_text[i]==' ':
             encrypted_text += ' '
         else:
-            position = letters.find(plain_text[i])
-            new_position = (key[0]*position + key[1]) % 26
+            new_position = key.find(plain_text[i])
             encrypted_text += letters[new_position]
     return encrypted_text,key
 
-def affineDecryptionWithKey(plain_text,key):
+def sustitutionEncryptionNoKey(plain_text):
+    encrypted_text = ''
+    letters_aux = letters
+    key = ''
+    for j in range(len(letters_aux)):    
+        choice = random.choice(letters_aux)
+        key += choice
+        letters_aux = letters_aux.replace(choice, "")
+    for i in range(len(plain_text)):
+        if plain_text[i]==' ':
+            encrypted_text += ' '
+        else:
+            new_position = key.find(plain_text[i])
+            encrypted_text += letters[new_position]
+    return encrypted_text,key
+
+def sustitutionDecryptionWithKey(plain_text,key):
     encrypted_text = plain_text
     decrypted_text = ''
     for i in range(len(encrypted_text)):
         if plain_text[i]==' ':
             decrypted_text += ' '
         else:
-            position = letters.find(encrypted_text[i])
-            new_position = (int(pow(key[0], -1, 26))*(position - key[1])) % 26
-            decrypted_text += letters[new_position]
+            new_position = letters.find(encrypted_text[i])
+            decrypted_text += key[new_position]
     return decrypted_text,key
-
