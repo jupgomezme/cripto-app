0fba1618fdb371c1fc90b7015be6c5177ac9a4e7
aerubianoma <aerubianoma@unal.edu.co>
2021-11-03 22:00:57 -0500

    added crypto analysis to cesar encryption
    I added the crypto analysis for the cesar encryption

--- api/frequencyTable.py
+++ api/frequencyTable.py
@@ -1,12 +1,14 @@
 import numpy as np
+
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-def printFrequencyTable(plain_text):
-    frequencyTable = np.zeros(26) 
+
+def frequencyTable(plain_text):
+    frequencyVec = np.zeros(26) 
+    table = {}
     for i in range(len(plain_text)):
         position = letters.find(plain_text[i])
-        frequencyTable[position] += 1
-    frequencyTable = frequencyTable * 1/26
-    print('Letter   |   Frequency')
-    for i in range(len(frequencyTable)):
-        print('   '+letters[i]+'     |   '+str(frequencyTable[i]))
-    return letters,frequencyTable
+        frequencyVec[position] += 1
+    frequencyVec = frequencyVec * 1/26
+    for i in range(len(frequencyVec)):
+        table[letters[i]] = frequencyVec[i]*100
+    return table
