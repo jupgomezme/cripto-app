4019dcfb1b8f239fab849466013aa333865be9b9
jp <juangomezcol.10@gmail.com>
2021-11-26 00:33:37 -0500

    first deployment to eb

--- application.py
+++ application.py
@@ -1,23 +1,21 @@
 from flask import Flask, request
 import json
-import os
 from displacement import cesarDecryptionNoKey, cesarDecryptionWithKey, cesarEncryptionNoKey, cesarEncryptionWithKey
 from frequencyTable import frequencyTable
 from flask_cors import CORS
-from dotenv import load_dotenv
-load_dotenv()
-HOST = os.environ.get("HOST")
-PORT = os.environ.get("PORT")
-ENV = os.environ.get("ENV")
 
+application = Flask(__name__)
+CORS(application)
 
-app = Flask(__name__)
-CORS(app)
+@application.route('/', methods=["GET"])
+def check_api():
+    return {
+        "message": "The api works!"
+    }
 
-@app.route('/', methods=['POST'])
+@application.route('/', methods=['POST'])
 def index():
     body = json.loads(request.data)
-    print("body:", body)
     algorithm = body["algorithm"]
     action = body["action"]
     data = body["data"]
@@ -40,19 +38,17 @@ def index():
     }
 
 
-@app.route('/frequency_table', methods=['POST'])
-def frequency_table():
-    body = json.loads(request.data)
-    data = body["data"]
-    frequency_table_ = frequencyTable(data)
-    return {
-        "frequency_table": list(frequency_table_)
-    }
+# @app.route('/frequency_table', methods=['POST'])
+# def frequency_table():
+#     body = json.loads(request.data)
+#     data = body["data"]
+#     frequency_table_ = frequencyTable(data)
+#     return {
+#         "frequency_table": list(frequency_table_)
+#     }
 
 
 if __name__ == "__main__":
-    if ENV == "dev":
-        app.run(debug=True, host=HOST, port=PORT)
-    elif ENV == "production":
-        from waitress import serve
-        serve(app, host=HOST, port=PORT)
+    application.debug = True
+    application.run()
+
