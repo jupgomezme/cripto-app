9af6b0f6c3bd431d8ddec59c84c37655693a5702
jp <juangomezcol.10@gmail.com>
2021-12-15 22:20:56 -0500

    deleting yaml file

--- lib/cdk-cripto-app-stack.ts
+++ lib/cdk-cripto-app-stack.ts
@@ -2,47 +2,45 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from "@aws-cdk/aws-lambda";
 import { PythonFunction } from "@aws-cdk/aws-lambda-python";
 import * as apigw from '@aws-cdk/aws-apigateway';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as efs from '@aws-cdk/aws-efs';
 import * as path from 'path';
 
-export class CdkCriptoAppBackendStack extends cdk.Stack {
+export class CdkCriptoAppBackStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "Vpc2", {
-      maxAzs: 2,
-    });
+    // const vpc = new ec2.Vpc(this, "Vpc2", {
+    //   maxAzs: 2,
+    // });
 
-    const fs = new efs.FileSystem(this, "FIleSystem2", {
-      vpc,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-    });
+    // const fs = new efs.FileSystem(this, "FIleSystem2", {
+    //   vpc,
+    //   removalPolicy: cdk.RemovalPolicy.DESTROY,
+    // });
 
-    const accessPoint = fs.addAccessPoint("AccessPoint2", {
-      createAcl: {
-        ownerGid: "1001",
-        ownerUid: "1001",
-        permissions: "750"
-      },
-      posixUser: {
-        gid: "1001",
-        uid: "1001"
-      }
-    })
+    // const accessPoint = fs.addAccessPoint("AccessPoint2", {
+    //   createAcl: {
+    //     ownerGid: "1001",
+    //     ownerUid: "1001",
+    //     permissions: "750"
+    //   },
+    //   posixUser: {
+    //     gid: "1001",
+    //     uid: "1001"
+    //   }
+    // })
 
-    const lambdaFunction = new PythonFunction(this, 'MyFunction2', {
-      timeout: cdk.Duration.seconds(900),
-      functionName: "MainFunction2",
+    const lambdaFunction = new PythonFunction(this, 'MyFunction', {
+      timeout: cdk.Duration.seconds(30),
+      functionName: "MainFunction",
       entry: path.join(__dirname, "./../src"), // required
       index: "index.py",
       handler: 'handler', // optional, defaults to 'handler'
       runtime: lambda.Runtime.PYTHON_3_8, // optional, defaults to lambda.Runtime.PYTHON_3_7
       // vpc,
       // filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, "/mnt/efs"),
-      memorySize: 1024,
-      vpc,
-      filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, "/mnt/efs"),
+      memorySize: 128,
+      // vpc,
+      // filesystem: lambda.FileSystem.fromEfsAccessPoint(accessPoint, "/mnt/efs"),
     });
 
     const api = new apigw.LambdaRestApi(this, 'Gateway2', {
