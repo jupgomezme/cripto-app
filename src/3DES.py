6d62b311e9c108041dda6f0d43b0083184ded113
aerubianoma <aerubianoma@unal.edu.co>
2022-01-10 12:30:38 -0500

    DES,3DES,SDEDS
    I add three new methods for the second term

---
+++ src/3DES.py
@@ -0,0 +1,29 @@
+from pyDes import CBC, PAD_PKCS5, triple_des
+import binascii
+ 
+def DESEncrypt(s,key):
+    """
+         Cifrado DES
+         : param s: cadena sin procesar
+         : return: cadena encriptada, hexadecimal
+    """
+    secret_key = key
+    k = triple_des(secret_key, CBC, pad=None, padmode=PAD_PKCS5)
+    en = k.encrypt(s, padmode=PAD_PKCS5)
+    return binascii.b2a_hex(en),key
+ 
+ 
+def DESDecrypt(s,key):
+    """
+         Descifrado DES
+         : param s: cadena encriptada, hexadecimal
+         : return: cadena descifrada
+    """
+    secret_key = key
+    k = triple_des(secret_key, CBC, pad=None, padmode=PAD_PKCS5)
+    de = k.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
+    return de,key
+
+print(DESEncrypt('HOLAMUNDO','ABCDEFGHDLKEGYTOLAKTREOP'))
+print(DESDecrypt('4540bde09edfbe27bf554f494a56f2ce','ABCDEFGHDLKEGYTOLAKTREOP'))
+
---
+++ src/DES.py
@@ -0,0 +1,30 @@
+from pyDes import des, CBC, PAD_PKCS5
+import binascii
+ 
+def DESEncrypt(s,key):
+    """
+         Cifrado DES
+         : param s: cadena sin procesar
+         : return: cadena encriptada, hexadecimal
+    """
+    secret_key = key
+    iv = secret_key
+    k = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    en = k.encrypt(s, padmode=PAD_PKCS5)
+    return binascii.b2a_hex(en),key
+ 
+ 
+def DESDecrypt(s,key):
+    """
+         Descifrado DES
+         : param s: cadena encriptada, hexadecimal
+         : return: cadena descifrada
+    """
+    secret_key = key
+    iv = secret_key
+    k = des(secret_key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    de = k.decrypt(binascii.a2b_hex(s), padmode=PAD_PKCS5)
+    return de,key
+
+print(DESEncrypt('HOLAMUNDO','AASBFURE'))
+print(DESDecrypt('36b617f26eaee1773c65571a822b29c9','AASBFURE'))
