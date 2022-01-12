91d2d5f77537268f531791ccd2c94e77ada33594
jupgomezme <juangomezcol.10@gmail.com>
2022-01-12 01:57:43 -0500

    updating endpoint

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const API_ENDPOINT = "http://0.0.0.0:8000/";
+const API_ENDPOINT = "http://0.0.0.0:3001/";
 
 const lettersAndSpace = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
 const numbersAndComma = "0123456789,"
